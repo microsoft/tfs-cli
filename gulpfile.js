03a86a623868961fdd610c4eb96c895a5b6198d7
Bryan MacFarlane <bryanmac@microsoft.com>
2015-07-22 06:26:33 -0400

    app scaffolding

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var fs = require('fs');
+var path = require('path');
+var gulp = require('gulp');
+var del = require('del');
+var mocha = require('gulp-mocha');
+var typescript = require('gulp-tsc');
+var minimist = require('minimist');
+
+gulp.task('compile', function(){
+  gulp.src(['src/**/*.ts'])
+    .pipe(typescript())
+    .pipe(gulp.dest('dest/'))
+});
+
+var buildRoot = path.join(__dirname, '_build');
+var appDest = path.join(buildRoot, 'app');
+var testDest = path.join(buildRoot, 'test');
+
+gulp.task('copy', ['clean'], function () {
+	return gulp.src(['package.json', './README.md', './app/tfx-cli.js'])
+		.pipe(gulp.dest(appDest));
+});
+
+gulp.task('compileApp', ['clean'], function () {
+	return gulp.src(['app/*.ts'])
+		.pipe(typescript({ declaration: false }))
+		.pipe(gulp.dest(appDest));
+});
+
+gulp.task('compileTests', ['clean'], function () {
+	return gulp.src(['tests/*.ts'])
+		.pipe(typescript({ declaration: false }))
+		.pipe(gulp.dest(testDest));
+});
+
+gulp.task('build', ['clean', 'compileApp', 'compileTests', 'copy']);
+
+gulp.task('testprep', function () {
+	return gulp.src(['tests/resources/*.*'])
+		.pipe(gulp.dest(path.join(testDest, 'resources')));
+});
+
+var mopts = {
+  boolean: 'ci',
+  string: 'suite',
+  default: { ci: false, suite: '*' }
+};
+
+var options = minimist(process.argv.slice(2), mopts);
+
+gulp.task('test', ['testprep'], function () {
+	var suitePath = path.join(testDest, '*.js');
+	console.log(suitePath);
+	if (options.suite !== '*') {
+		suitePath = path.join(testPath, options.suite + '.js');
+	}
+
+	return gulp.src([suitePath])
+		.pipe(mocha({ reporter: 'spec', ui: 'bdd', useColors: !options.ci }));
+});
+
+gulp.task('clean', function (done) {
+	del([buildRoot], done);
+});
+
+gulp.task('default', ['build']);
---
+++ tests/commandline.ts
@@ -0,0 +1,32 @@
+/// <reference path="../definitions/mocha.d.ts"/>
+/// <reference path="../definitions/Q.d.ts"/>
+/// <reference path="../definitions/node.d.ts"/>
+import Q = require('q');
+import assert = require('assert');
+
+describe('tfx-cli', function() {
+
+	before((done) => {
+		// init here
+		done();
+	});
+
+	after(function() {
+
+	});
+
+	describe('Command Line Parsing', function() {
+		it('It succeeds', (done) => {
+			this.timeout(500);
+
+			assert(true, 'true is not true?');
+			done();
+		})		
+		it('It succeeds again', (done) => {
+			this.timeout(500);
+
+			assert(true, 'true is not true?');
+			done();
+		})
+	});
+});
