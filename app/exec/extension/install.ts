069e0eb45ab5f508e4d3e9ac550237ac8d012c39
bfcamara <bruno.camara@gmail.com>
2016-03-01 10:19:11 +0000

    generate by hand Locations and Accounts API while they are not included in vso-node-api use Locations service to get SPS base URL

--- app/exec/extension/install.ts
+++ app/exec/extension/install.ts
@@ -8,13 +8,13 @@ import trace = require("../../lib/trace");
 import GalleryInterfaces = require("vso-node-api/interfaces/GalleryInterfaces");
 import gallerym = require("vso-node-api/GalleryApi");
 
-const VSTS_SPS_BASE_URL = "https://app.vssps.visualstudio.com";
+const SPS_INSTANCE_TYPE = "951917AC-A960-4999-8464-E3F0AA25B381";
 
 export function getCommand(args: string[]): TfCommand<extBase.ExtensionArguments, ExtensionInstallResult> {
     return new ExtensionInstall(args);
 }
 
-interface Account {
+interface TargetAccount {
     accountName: string;
     accountId: string;
 }
@@ -139,20 +139,22 @@ export class ExtensionInstall extends extBase.ExtensionBase<ExtensionInstallResu
         });
     }
 
-    private _resolveInstallationAccounts(galleryApi: gallerym.IQGalleryApi, accounts: string[]): Q.Promise<Account[]> {
+    private _resolveInstallationAccounts(galleryApi: gallerym.IQGalleryApi, accounts: string[]): Q.Promise<TargetAccount[]> {
         if (!accounts || accounts.length === 0) {
             return Q.resolve([]);
         }
 
-        const result: Q.Deferred<Account[]> = Q.defer<Account[]>();
+        const result: Q.Deferred<TargetAccount[]> = Q.defer<TargetAccount[]>();
 
         // Use connectionData service to determine userId
+        trace.debug("Connecting to service to get user");
         galleryApi.connect()
             .then((data: any) => {
                 if (!data || !data.authenticatedUser || !data.authenticatedUser.id) {
                     throw new Error("Cannot determine authenticated user in order to resolve accounts for which is an owner or a member of.");
                 }
 
+                trace.debug("Installation User Id:" + data.authenticatedUser.id);
                 return this._getAccountsByMemberId(data.authenticatedUser.id);
             })
             .then((userAccounts: Account[]) => {
@@ -162,7 +164,7 @@ export class ExtensionInstall extends extBase.ExtensionBase<ExtensionInstallResu
                     return resolvedAccount && resolvedAccount.length > 0 ? resolvedAccount[0].accountId : undefined;
                 };
 
-                return result.resolve(accounts.map(accountName => { return { accountId: getAccountId(accountName), accountName: accountName }; }));
+                return result.resolve(accounts.map(accountName => { return <TargetAccount>{ accountId: getAccountId(accountName), accountName: accountName }; }));
             })
             .fail(err => {
                 result.reject(err);
@@ -194,7 +196,7 @@ export class ExtensionInstall extends extBase.ExtensionBase<ExtensionInstallResu
             .fail((err) => {
                 // Ignore error 'AlreadyInstalled'
                 if (this._isAlreadyInstalledError(err)) {
-                    trace.debug(`Succeeded GalleryApi.requestAcquisition in ${accountName}: Alreeady installed`);
+                    trace.debug(`Succeeded GalleryApi.requestAcquisition in ${accountName}: Already installed`);
                     accountReport.setInstalled("Extension already installed");
                     return accountReport;
                 }
@@ -211,45 +213,143 @@ export class ExtensionInstall extends extBase.ExtensionBase<ExtensionInstallResu
     }
 
     private _getAccountsByMemberId(userId: string): Q.Promise<Account[]> {
-        const accountsBaseUrl = this._getAccountsServiceBaseUrl(this.webApi.serverUrl);
-        const accountsApi = new AccountsApi(accountsBaseUrl, [this.webApi.authHandler]);
-        return accountsApi.getAccountsByMemberId(userId);
+        trace.debug("Getting Accounts to user:" + userId);
+        return this._getAccountsServiceBaseUrl()
+            .then(accountsBaseUrl => {
+                const accountsApi = new AccountsApi(accountsBaseUrl, [this.webApi.authHandler]);
+                return accountsApi.getAccounts(undefined, undefined, userId);
+            });
     }
 
-
     /**
-     * Determine the Accounts URL based on the service URL.
-     * TODO: Check if it is possible to do it using the REST API.
+     * Determine the Accounts URL based by using the Location Service.
      *
      * @param  {string} serviceUrl
      * @returns string
      */
-    private _getAccountsServiceBaseUrl(serviceUrl: string): string {
-        // When hosted, return SPS URL
-        if (serviceUrl.toLowerCase().indexOf(".visualstudio.com") > 0) {
-            return VSTS_SPS_BASE_URL;
-        }
-
-        // Otherwise return the original serviceUrl
-        return serviceUrl;
+    private _getAccountsServiceBaseUrl(): Q.Promise<string> {
+        trace.debug("Resolving SPS URL");
+        const locationsApi = new LocationsApi(this.webApi.serverUrl, [this.webApi.authHandler]);
+        return locationsApi.getServiceDefinition("LocationService2", SPS_INSTANCE_TYPE)
+            .then(serviceDefinition => {
+                const publicMapping = serviceDefinition.locationMappings.filter(l => l.accessMappingMoniker === "PublicAccessMapping");
+                if (!publicMapping || publicMapping.length === 0) {
+                    throw new Error("Cannot determine base URL for Accounts service.");
+                }
+                trace.debug("SPS URL resolved: " + publicMapping[0].location);
+                return publicMapping[0].location;
+            });
     }
 }
 
-
 /**
- * TODO: Remove this as soon as it is included in the vso-node-api module
+ * ------------------------------------------
+ * VSTS APIs NOT PROVIDED YET BY VSO-NODE-API
+ *-------------------------------------------
  *
  */
 
 import VsoBaseInterfaces = require("vso-node-api/interfaces/common/VsoBaseInterfaces");
 import { ClientApiBase } from  "vso-node-api/ClientApiBases";
 
+/**
+ * --------------
+ *  ACCOUNTS API (TODO: REMOVE IT AS SOON AS IT IS INCLUDED IN VSO-NODE-API)
+ * --------------
+ */
+
+enum AccountStatus {
+    None = 0,
+    /**
+     * This hosting account is active and assigned to a customer.
+     */
+    Enabled = 1,
+    /**
+     * This hosting account is disabled.
+     */
+    Disabled = 2,
+    /**
+     * This account is part of deletion batch and scheduled for deletion.
+     */
+    Deleted = 3,
+}
+
+enum AccountType {
+    Personal = 0,
+    Organization = 1,
+}
+
+interface Account {
+    /**
+     * Identifier for an Account
+     */
+    accountId: string;
+    /**
+     * Name for an account
+     */
+    accountName: string;
+    /**
+     * Owner of account
+     */
+    accountOwner: string;
+    /**
+     * Current account status
+     */
+    accountStatus: AccountStatus;
+    /**
+     * Type of account: Personal, Organization
+     */
+    accountType: AccountType;
+    /**
+     * Uri for an account
+     */
+    accountUri: string;
+    /**
+     * Who created the account
+     */
+    createdBy: string;
+    /**
+     * Date account was created
+     */
+    createdDate: Date;
+    /**
+     * Identity of last person to update the account
+     */
+    lastUpdatedBy: string;
+    /**
+     * Date account was last updated
+     */
+    lastUpdatedDate: Date;
+    /**
+     * Namespace for an account
+     */
+    namespaceId: string;
+    /**
+     * Organization that created the account
+     */
+    organizationName: string;
+    /**
+     * Extended properties
+     */
+    properties: any;
+    /**
+     * Reason for current status
+     */
+    statusReason: string;
+}
+
 class AccountsApi extends ClientApiBase {
     constructor(baseUrl: string, handlers: VsoBaseInterfaces.IRequestHandler[]) {
         super(baseUrl, handlers, "tfx-cli");
     }
 
-    public getAccountsByMemberId(memberId: string): Q.Promise<Account[]> {
+    public getAccounts(
+        creatorId?: string,
+        ownerId?: string,
+        memberId?: string,
+        includeOwner?: boolean,
+        properties?: string,
+        includeDisabledAccounts?: boolean): Q.Promise<Account[]> {
         const deferred = Q.defer<Account[]>();
 
         const onResult = (err: any, statusCode: number, accounts: Account[]) => {
@@ -262,11 +362,121 @@ class AccountsApi extends ClientApiBase {
             }
         };
 
-        this.vsoClient.getVersioningData("2.0", "account", "229a6a53-b428-4ffb-a835-e8f36b5b4b1e", null, { memberId: memberId })
+        const queryValues: any = {
+            creatorId: creatorId,
+            ownerId: ownerId,
+            memberId: memberId,
+            includeOwner: includeOwner,
+            properties: properties,
+            includeDisabledAccounts: includeDisabledAccounts,
+        };
+
+        this.vsoClient.getVersioningData("2.0", "account", "229a6a53-b428-4ffb-a835-e8f36b5b4b1e", null, queryValues)
             .then(versioningData => {
                 const url: string = versioningData.requestUrl;
                 const apiVersion: string = versioningData.apiVersion;
                 const serializationData = { responseIsCollection: true };
+
+                this.restClient.getJson(url, apiVersion, null, serializationData, onResult);
+            })
+            .fail(error => {
+                onResult(error, error.statusCode, null);
+            });
+
+        return deferred.promise;
+    }
+}
+
+/**
+ * ---------------
+ *  LOCATIONS API (TODO: REMOVE IT AS SOON AS IT IS INCLUDED IN VSO-NODE-API)
+ * ---------------
+ */
+
+enum InheritLevel {
+    None = 0,
+    Deployment = 1,
+    Account = 2,
+    Collection = 4,
+    All = 7,
+}
+
+enum RelativeToSetting {
+    Context = 0,
+    WebApplication = 2,
+    FullyQualified = 3,
+}
+
+enum ServiceStatus {
+    Assigned = 0,
+    Active = 1,
+    Moving = 2,
+}
+
+interface LocationMapping {
+    accessMappingMoniker: string;
+    location: string;
+}
+
+interface ServiceDefinition {
+    description: string;
+    displayName: string;
+    identifier: string;
+    inheritLevel: InheritLevel;
+    locationMappings: LocationMapping[];
+    /**
+     * Maximum api version that this resource supports (current server version for this resource). Copied from ApiResourceLocation.
+     */
+    maxVersion: string;
+    /**
+     * Minimum api version that this resource supports. Copied from ApiResourceLocation.
+     */
+    minVersion: string;
+    parentIdentifier: string;
+    parentServiceType: string;
+    properties: any;
+    relativePath: string;
+    relativeToSetting: RelativeToSetting;
+    /**
+     * The latest version of this resource location that is in "Release" (non-preview) mode. Copied from ApiResourceLocation.
+     */
+    releasedVersion: string;
+    /**
+     * The current resource version supported by this resource location. Copied from ApiResourceLocation.
+     */
+    resourceVersion: number;
+    serviceType: string;
+    status: ServiceStatus;
+}
+
+class LocationsApi extends ClientApiBase {
+    constructor(baseUrl: string, handlers: VsoBaseInterfaces.IRequestHandler[]) {
+        super(baseUrl, handlers, "tfx-cli");
+    }
+
+    public getServiceDefinition(serviceType: string, identifier: string): Q.Promise<ServiceDefinition> {
+        const deferred = Q.defer<ServiceDefinition>();
+
+        const onResult = (err: any, statusCode: number, serviceDefinition: ServiceDefinition) => {
+            if (err) {
+                err.statusCode = statusCode;
+                deferred.reject(err);
+            }
+            else {
+                deferred.resolve(serviceDefinition);
+            }
+        };
+
+        const routeValues = {
+            serviceType: serviceType,
+            identifier: identifier
+        };
+
+        this.vsoClient.getVersioningData("3.0-preview.1", "location", "d810a47d-f4f4-4a62-a03f-fa1860585c4c", routeValues)
+            .then(versioningData => {
+                const url: string = versioningData.requestUrl;
+                const apiVersion: string = versioningData.apiVersion;
+                const serializationData = { responseIsCollection: false };
 
                 this.restClient.getJson(url, apiVersion, null, serializationData, onResult);
             })
