d75f9b10c0933b9dc35d9359e46e0d997f4b83c5
sfeldman <shani.feldman@intel.com>
2016-06-13 21:33:24 +0300

    first try to get agent details

---
+++ app/exec/build/agent.ts
@@ -0,0 +1,44 @@
+import { TfCommand } from "../../lib/tfcommand";
+import args = require("../../lib/arguments");
+import buildBase = require("./default");
+import agentClient = require("vso-node-api/TaskAgentApiBase");
+import taskAgentContracts = require("vso-node-api/interfaces/TaskAgentInterfaces");
+import trace = require("../../lib/trace");
+
+export function getCommand(args: string[]): Agent {
+	return new Agent(args);
+}
+
+export class Agent extends buildBase.BuildBase<buildBase.BuildArguments, taskAgentContracts.TaskAgent> {
+	protected description = "Show task agent details.";
+
+	protected getHelpArgs(): string[] {
+		return ["poolId", "agentId"];
+	}
+
+	public exec(): Q.Promise<taskAgentContracts.TaskAgent> {
+		trace.debug("agent.exec");
+		var agentapi: agentClient.IQTaskAgentApiBase = this.webApi.getQTaskAgentApi();
+		return this.commandArgs.poolId.val().then((pool) => {
+			trace.info("getting agent pool:	 %s",pool);
+			return this.commandArgs.agentId.val().then((agent) => {
+				trace.info("getting agent	  : %s", agent);
+				return agentapi.getAgent(pool,agent,true,true,null);
+			});
+		});
+
+	}
+
+	public friendlyOutput(agent: taskAgentContracts.TaskAgent): void {
+		//if (!agent) {
+		//	throw new Error("no agent / pool supplied");
+		//}
+		
+		trace.println();
+		trace.info("Agent id        : %s", agent.id ? agent.id : "unknown");
+		trace.info("Agent name      : %s", agent.name ? agent.name : "unknown");
+		trace.info("Version         : %s", agent.version ? agent.version : "unknown");
+		trace.info("status          : %s", agent.status[agent.status]);
+		trace.info("queue time      : %s", agent.enabled ? agent.enabled : "unknown");
+	}
+}
