003a0b6c3ea1a48ac0535f85b1f13c217fb28449
sfeldman <shani.feldman@intel.com>
2016-07-18 21:00:30 +0300

    create package saves version to file for artifactore deploy

--- create_package.sh
+++ create_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-version=$(cat package.json|grep version|sed s/\"version\"\:\ \"//g|sed s/\"\,//g|sed s/\ //g)
-echo $version
+export version=$(cat package.json|grep version|sed s/\"version\"\:\ \"//g|sed s/\"\,//g|sed s/\ //g)
+echo $version > ./version.txt
 mkdir -p tfx-cli
 cp -r node_modules tfx-cli/
 cp -r _build tfx-cli/
