e38bbdbf2e7fd2ae8e54286141c684a5119d25e0
Trevor Gau <trgau@microsoft.com>
2016-09-06 22:57:33 -0400

    Update vso-node-api to latest and redo extension installation to use EMS endpoint.

--- app/exec/extension/install.ts
+++ app/exec/extension/install.ts
@@ -1,11 +1,14 @@
 import { TfCommand } from "../../lib/tfcommand";
 import args = require("../../lib/arguments");
+import colors = require("colors");
 import extBase = require("./default");
 import extInfo = require("./_lib/extensioninfo");
 import Q = require("q");
 import trace = require("../../lib/trace");
 import GalleryInterfaces = require("vso-node-api/interfaces/GalleryInterfaces");
 import gallerym = require("vso-node-api/GalleryApi");
+import emsm = require("vso-node-api/ExtensionManagementApi");
+import EmsInterfaces = require("vso-node-api/interfaces/ExtensionManagementInterfaces");
 
 const SPS_INSTANCE_TYPE = "951917AC-A960-4999-8464-E3F0AA25B381";
 
@@ -38,30 +41,35 @@ export class AccountInstallReport {
 	}
 }
 
-export class ExtensionInstallResult {
-	constructor(
-		public itemId: string,
-		public accountReports: AccountInstallReport[]) {
-	}
+// export class ExtensionInstallResult {
+// 	constructor(
+// 		public itemId: string,
+// 		public accountReports: AccountInstallReport[]) {
+// 	}
 
-	public hasErrors(): boolean {
-		return this.accountReports.some(t => !t.installed);
-	}
+// 	public hasErrors(): boolean {
+// 		return this.accountReports.some(t => !t.installed);
+// 	}
 
-	public toString() {
-		let result: string;
-		const status: string = this.hasErrors() ? "Failed" : "Succeeded";
+// 	public toString() {
+// 		let result: string;
+// 		const status: string = this.hasErrors() ? "Failed" : "Succeeded";
 
-		result = `=== Installation ${status} === ` +
-			`\n  - ItemId: ${this.itemId}` +
-			"\n  - Accounts:";
+// 		result = `=== Installation ${status} === ` +
+// 			`\n  - ItemId: ${this.itemId}` +
+// 			"\n  - Accounts:";
 
-		this.accountReports.forEach(t => {
-			result += `\n	- ${t.accountName}: [${t.installed ? "OK" : `Failed: ${t.reason}`}]`;
-		});
+// 		this.accountReports.forEach(t => {
+// 			result += `\n	- ${t.accountName}: [${t.installed ? "OK" : `Failed: ${t.reason}`}]`;
+// 		});
 
-		return result;
-	}
+// 		return result;
+// 	}
+// }
+
+export interface ExtensionInstallResult {
+	accounts: {[account: string]: {installed: boolean; issues: string}};
+	extension: string;
 }
 
 export class ExtensionInstall extends extBase.ExtensionBase<ExtensionInstallResult> {
@@ -82,48 +90,56 @@ export class ExtensionInstall extends extBase.ExtensionBase<ExtensionInstallResu
 
 	public exec(): Promise<ExtensionInstallResult> {
 		// Read extension info from arguments
+		const result: ExtensionInstallResult = {accounts: {}, extension: null};
 		return this._getExtensionInfo()
 			.then(extInfo => {
 				const itemId = `${extInfo.publisher}.${extInfo.id}`;
 				const galleryApi = this.webApi.getGalleryApi(this.webApi.serverUrl);
-				
-				
+
+				result.extension = itemId;
 
 				// Read accounts from arguments and resolve them to get its accountIds
-				return this.commandArgs.accounts.val()
-					.then((accounts) => {
-						return this._resolveInstallationAccounts(galleryApi, accounts);
-					})
-					.then(accounts => {
-						// Install extension in each account
-						const installations = [...accounts].map(account => this._installExtension(galleryApi, itemId, account.accountId, account.accountName));
-						return Promise.all(installations);
-					})
-					.then(targetReports => {
-						// Process installation results. We reject if exists at least one that was not installed successfully
-						const result = new ExtensionInstallResult(itemId, targetReports);
-						if (result.hasErrors()) {
-							return Q.reject<ExtensionInstallResult>(result);
-						}
-
-						// All succeeded. Return resolved.
-						return result;
+				return this.commandArgs.accounts.val().then(accounts => {
+					// Install extension in each account
+					const installations = [...accounts].map((account): Promise<[string, EmsInterfaces.InstalledExtension]> => {
+						const emsApi = this.webApi.getExtensionManagementApi(this.getEmsAccountUrl(this.webApi.serverUrl, account));
+						return emsApi.installExtensionByName(extInfo.publisher, extInfo.id).then(installation => [account, installation]);
 					});
+					return Promise.all(installations);
+				}).then(installations => {
+					installations.forEach(installation => {
+						const account: string = installation[0];
+						const installedExtension: EmsInterfaces.InstalledExtension = installation[1];
+						const installationResult = {installed: true, issues: null};
+						if (installedExtension.installState.installationIssues && installedExtension.installState.installationIssues.length > 0) {
+							installationResult.installed = false;
+							installationResult.issues = `The following issues were encountered installing to ${account}: 
+${installedExtension.installState.installationIssues.map(i => " - " + i).join("\n")}`
+						}
+						result.accounts[account] = installationResult;
+					});
+					return result;
+				});
 			});
+	}
+
+	private getEmsAccountUrl(marketplaceUrl: string, accountName: string) {
+		if (marketplaceUrl.toLocaleLowerCase().indexOf("marketplace.visualstudio.com") >= 0) {
+			return `https://${accountName}.extmgmt.visualstudio.com`;
+		}
+		if (marketplaceUrl.toLocaleLowerCase().indexOf("me.tfsallin.net") >= 0) {
+			return marketplaceUrl.toLocaleLowerCase().indexOf("https://") === 0 ?
+				`https://${accountName}.me.tfsallin.net:8781` :
+				`http://${accountName}.me.tfsallin.net:8780`;
+		}
+		return marketplaceUrl;
 	}
 
 	protected friendlyOutput(data: ExtensionInstallResult): void {
 		trace.success("\n=== Completed operation: install extension ===");
-		trace.info(` - Installed extension ${data.itemId} in:`);
-
-		if (data.accountReports && data.accountReports.length > 0) {
-			data.accountReports.forEach((report) => {
-				trace.info("   - " + report.accountName);
-			});
-		} else {
-			trace.info("No accounts specified");
-		}
-
+		Object.keys(data.accounts).forEach(a => {
+			trace.info(`- ${a}: ${data.accounts[a].installed ? colors.green("success") : colors.red(data.accounts[a].issues)}`);
+		});
 	}
 
 	private _getExtensionInfo(): Promise<extInfo.CoreExtInfo> {
@@ -143,348 +159,5 @@ export class ExtensionInstall extends extBase.ExtensionBase<ExtensionInstallResu
 
 			return extInfoPromise;
 		});
-	}
-
-	private _resolveInstallationAccounts(galleryApi: gallerym.IGalleryApi, accounts: string[]): Promise<TargetAccount[]> {
-		if (!accounts || accounts.length === 0) {
-			return Q.resolve([]);
-		}
-
-		const result: Q.Deferred<TargetAccount[]> = Q.defer<TargetAccount[]>();
-
-		// Use connectionData service to determine userId
-		trace.debug("Connecting to service to get user");
-		galleryApi.connect().then((data: any) => {
-			if (!data || !data.authenticatedUser || !data.authenticatedUser.id) {
-				throw new Error("Cannot determine authenticated user in order to resolve accounts for which is an owner or a member of.");
-			}
-
-			trace.debug("Installation User Id:" + data.authenticatedUser.id);
-			return this._getAccountsByMemberId(data.authenticatedUser.id);
-		}).then((userAccounts: Account[]) => {
-
-			const getAccountId = (accountName: string): string => {
-				const resolvedAccount = userAccounts.filter(a => a.accountName.toLowerCase() === accountName.toLowerCase());
-				return resolvedAccount && resolvedAccount.length > 0 ? resolvedAccount[0].accountId : undefined;
-			};
-
-			return result.resolve(accounts.map(accountName => { return <TargetAccount>{ accountId: getAccountId(accountName), accountName: accountName }; }));
-		}).catch(err => {
-			result.reject(err);
-		});
-
-		return result.promise;
-	}
-
-	private _installExtension(galleryApi: gallerym.IGalleryApi, itemId: string, accountId: string, accountName: string): Promise<AccountInstallReport> {
-		const accountReport: AccountInstallReport = new AccountInstallReport(itemId, accountName, accountId);
-
-		if (!accountId) {
-			accountReport.setError(`Cannot install extension into account ${accountName} because is not an acccount for which the installation user is owner or a member of.`);
-			return Q.resolve(accountReport);
-		}
-
-		return galleryApi.requestAcquisition(<GalleryInterfaces.ExtensionAcquisitionRequest>{
-			assignmentType: GalleryInterfaces.AcquisitionAssignmentType.None,
-			billingId: null,
-			itemId: itemId,
-			operationType: GalleryInterfaces.AcquisitionOperationType.Install,
-			targets: [accountId]
-		}).then(report => {
-			trace.debug(`Succeeded GalleryApi.requestAcquisition in ${accountName}: ${report}`);
-			accountReport.installed = true;
-			return accountReport;
-		}).catch((err) => {
-			// Ignore error 'AlreadyInstalled'
-			if (this._isAlreadyInstalledError(err)) {
-				trace.debug(`Succeeded GalleryApi.requestAcquisition in ${accountName}: Already installed`);
-				accountReport.setInstalled("Extension already installed");
-				return accountReport;
-			}
-
-			trace.error(`Failed GalleryApi.requestAcquisition for account ${accountName}: ${err}`);
-			accountReport.setError(err);
-			return accountReport;
-		});
-	}
-
-	private _isAlreadyInstalledError(err): boolean {
-		// 409 - Conflict: means extension already installed
-		return err.statusCode === 409;
-	}
-
-	private _getAccountsByMemberId(userId: string): Promise<Account[]> {
-		trace.debug("Getting Accounts to user:" + userId);
-		return this._getAccountsServiceBaseUrl()
-			.then(accountsBaseUrl => {
-				const accountsApi = new AccountsApi(accountsBaseUrl, [this.webApi.authHandler]);
-				return accountsApi.getAccounts(undefined, undefined, userId);
-			});
-	}
-
-	/**
-	 * Determine the Accounts URL based by using the Location Service.
-	 *
-	 * @param  {string} serviceUrl
-	 * @returns string
-	 */
-	private _getAccountsServiceBaseUrl(): Promise<string> {
-		trace.debug("Resolving SPS URL");
-		const locationsApi = new LocationsApi(this.webApi.serverUrl, [this.webApi.authHandler]);
-		return locationsApi.getServiceDefinition("LocationService2", SPS_INSTANCE_TYPE)
-			.then(serviceDefinition => {
-				const publicMapping = serviceDefinition.locationMappings.filter(l => l.accessMappingMoniker === "PublicAccessMapping");
-				if (!publicMapping || publicMapping.length === 0) {
-					throw new Error("Cannot determine base URL for Accounts service.");
-				}
-				trace.debug("SPS URL resolved: " + publicMapping[0].location);
-				return publicMapping[0].location;
-			});
-	}
-}
-
-/**
- * ------------------------------------------
- * VSTS APIs NOT PROVIDED YET BY vso-node-api
- *-------------------------------------------
- *
- */
-
-import VsoBaseInterfaces = require("vso-node-api/interfaces/common/VsoBaseInterfaces");
-import { ClientApiBase } from  "vso-node-api/ClientApiBases";
-
-/**
- * --------------
- *  ACCOUNTS API (TODO: REMOVE IT AS SOON AS IT IS INCLUDED IN vso-node-api)
- * --------------
- */
-
-enum AccountStatus {
-	None = 0,
-	/**
-	 * This hosting account is active and assigned to a customer.
-	 */
-	Enabled = 1,
-	/**
-	 * This hosting account is disabled.
-	 */
-	Disabled = 2,
-	/**
-	 * This account is part of deletion batch and scheduled for deletion.
-	 */
-	Deleted = 3,
-}
-
-enum AccountType {
-	Personal = 0,
-	Organization = 1,
-}
-
-interface Account {
-	/**
-	 * Identifier for an Account
-	 */
-	accountId: string;
-	/**
-	 * Name for an account
-	 */
-	accountName: string;
-	/**
-	 * Owner of account
-	 */
-	accountOwner: string;
-	/**
-	 * Current account status
-	 */
-	accountStatus: AccountStatus;
-	/**
-	 * Type of account: Personal, Organization
-	 */
-	accountType: AccountType;
-	/**
-	 * Uri for an account
-	 */
-	accountUri: string;
-	/**
-	 * Who created the account
-	 */
-	createdBy: string;
-	/**
-	 * Date account was created
-	 */
-	createdDate: Date;
-	/**
-	 * Identity of last person to update the account
-	 */
-	lastUpdatedBy: string;
-	/**
-	 * Date account was last updated
-	 */
-	lastUpdatedDate: Date;
-	/**
-	 * Namespace for an account
-	 */
-	namespaceId: string;
-	/**
-	 * Organization that created the account
-	 */
-	organizationName: string;
-	/**
-	 * Extended properties
-	 */
-	properties: any;
-	/**
-	 * Reason for current status
-	 */
-	statusReason: string;
-}
-
-class AccountsApi extends ClientApiBase {
-	constructor(baseUrl: string, handlers: VsoBaseInterfaces.IRequestHandler[]) {
-		super(baseUrl, handlers, "tfx-cli");
-	}
-
-	public getAccounts(
-		creatorId?: string,
-		ownerId?: string,
-		memberId?: string,
-		includeOwner?: boolean,
-		properties?: string,
-		includeDisabledAccounts?: boolean): Promise<Account[]> {
-		const deferred = Q.defer<Account[]>();
-
-		const onResult = (err: any, statusCode: number, accounts: Account[]) => {
-			if (err) {
-				err.statusCode = statusCode;
-				deferred.reject(err);
-			}
-			else {
-				deferred.resolve(accounts);
-			}
-		};
-
-		const queryValues: any = {
-			creatorId: creatorId,
-			ownerId: ownerId,
-			memberId: memberId,
-			includeOwner: includeOwner,
-			properties: properties,
-			includeDisabledAccounts: includeDisabledAccounts,
-		};
-
-		this.vsoClient.getVersioningData("2.0", "account", "229a6a53-b428-4ffb-a835-e8f36b5b4b1e", null, queryValues)
-			.then(versioningData => {
-				const url: string = versioningData.requestUrl;
-				const apiVersion: string = versioningData.apiVersion;
-				const serializationData = { responseIsCollection: true };
-
-				this.restClient.getJson(url, apiVersion, null, serializationData, onResult);
-			})
-			.fail(error => {
-				onResult(error, error.statusCode, null);
-			});
-
-		return deferred.promise;
-	}
-}
-
-/**
- * ---------------
- *  LOCATIONS API (TODO: REMOVE IT AS SOON AS IT IS INCLUDED IN vso-node-api)
- * ---------------
- */
-
-enum InheritLevel {
-	None = 0,
-	Deployment = 1,
-	Account = 2,
-	Collection = 4,
-	All = 7,
-}
-
-enum RelativeToSetting {
-	Context = 0,
-	WebApplication = 2,
-	FullyQualified = 3,
-}
-
-enum ServiceStatus {
-	Assigned = 0,
-	Active = 1,
-	Moving = 2,
-}
-
-interface LocationMapping {
-	accessMappingMoniker: string;
-	location: string;
-}
-
-interface ServiceDefinition {
-	description: string;
-	displayName: string;
-	identifier: string;
-	inheritLevel: InheritLevel;
-	locationMappings: LocationMapping[];
-	/**
-	 * Maximum api version that this resource supports (current server version for this resource). Copied from ApiResourceLocation.
-	 */
-	maxVersion: string;
-	/**
-	 * Minimum api version that this resource supports. Copied from ApiResourceLocation.
-	 */
-	minVersion: string;
-	parentIdentifier: string;
-	parentServiceType: string;
-	properties: any;
-	relativePath: string;
-	relativeToSetting: RelativeToSetting;
-	/**
-	 * The latest version of this resource location that is in "Release" (non-preview) mode. Copied from ApiResourceLocation.
-	 */
-	releasedVersion: string;
-	/**
-	 * The current resource version supported by this resource location. Copied from ApiResourceLocation.
-	 */
-	resourceVersion: number;
-	serviceType: string;
-	status: ServiceStatus;
-}
-
-class LocationsApi extends ClientApiBase {
-	constructor(baseUrl: string, handlers: VsoBaseInterfaces.IRequestHandler[]) {
-		super(baseUrl, handlers, "tfx-cli");
-	}
-
-	public getServiceDefinition(serviceType: string, identifier: string): Promise<ServiceDefinition> {
-		const deferred = Q.defer<ServiceDefinition>();
-
-		const onResult = (err: any, statusCode: number, serviceDefinition: ServiceDefinition) => {
-			if (err) {
-				err.statusCode = statusCode;
-				deferred.reject(err);
-			}
-			else {
-				deferred.resolve(serviceDefinition);
-			}
-		};
-
-		const routeValues = {
-			serviceType: serviceType,
-			identifier: identifier
-		};
-
-		this.vsoClient.getVersioningData("3.0-preview.1", "location", "d810a47d-f4f4-4a62-a03f-fa1860585c4c", routeValues)
-			.then(versioningData => {
-				const url: string = versioningData.requestUrl;
-				const apiVersion: string = versioningData.apiVersion;
-				const serializationData = { responseIsCollection: false };
-
-				this.restClient.getJson(url, apiVersion, null, serializationData, onResult);
-			})
-			.fail(error => {
-				onResult(error, error.statusCode, null);
-			});
-
-		return deferred.promise;
 	}
 }
