bcdd787a6c29a6b2f9c31574b165714750a75118
sfeldman <shani.feldman@intel.com>
2017-03-14 11:30:45 +0200

    initial handlig (list, show) for service Endpoints

---
+++ app/exec/endpoints/list.ts
@@ -0,0 +1,51 @@
+import { TfCommand, CoreArguments } from "../../lib/tfcommand";
+import buildContracts = require('vso-node-api/interfaces/BuildInterfaces');
+import args = require("../../lib/arguments");
+import trace = require('../../lib/trace');
+import taskAgentContracts = require("vso-node-api/interfaces/TaskAgentInterfaces");
+import agentClient = require("vso-node-api/TaskAgentApiBase");
+import corem = require('vso-node-api/CoreApi');
+
+export function getCommand(args: string[]): ListEndpoints {
+    return new ListEndpoints(args);
+}
+
+export class ListEndpoints extends TfCommand<CoreArguments, taskAgentContracts.ServiceEndpoint[]> {
+    protected serverCommand = true;
+    protected description = "Get a list of build definitions";
+
+    protected getHelpArgs(): string[] {
+        return ["project"];
+    }
+
+    public exec(): Promise<taskAgentContracts.ServiceEndpoint[]> {
+        var agentapi: agentClient.ITaskAgentApiBase = this.webApi.getTaskAgentApi(this.connection.getCollectionUrl());
+        var coreapi:corem.ICoreApi = this.webApi.getCoreApi(this.connection.getCollectionUrl())
+        trace.debug("Searching for Service Endpoints ...");
+        return this.commandArgs.project.val().then((project) => {
+            return coreapi.getProject(project).then((projectObj) =>{               
+                return agentapi.getServiceEndpoints(projectObj.id).then((endpoints) => {
+                    trace.debug("Retrieved " + endpoints.length + " build endpoints from server.");
+                    return endpoints;
+                });
+            });
+        });
+    }
+
+    public friendlyOutput(data: taskAgentContracts.ServiceEndpoint[]): void {
+        if (!data) {
+            throw new Error('no definitions supplied');
+        }
+
+        if (!(data instanceof Array)) {
+            throw new Error('expected an array of definitions');
+        }
+
+        data.forEach((endpoint) => {
+            trace.println();
+            trace.info('name    : %s',endpoint.name);
+            trace.info('id      : %s',endpoint.id);
+            trace.info('type    : %s',endpoint.type);
+        });
+    }
+}
---
+++ app/exec/endpoints/show.ts
@@ -0,0 +1,57 @@
+import { TfCommand, CoreArguments } from "../../lib/tfcommand";
+import buildContracts = require('vso-node-api/interfaces/BuildInterfaces');
+import args = require("../../lib/arguments");
+import trace = require('../../lib/trace');
+import taskAgentContracts = require("vso-node-api/interfaces/TaskAgentInterfaces");
+import agentClient = require("vso-node-api/TaskAgentApiBase");
+import corem = require('vso-node-api/CoreApi');
+
+export function getCommand(args: string[]): ShowEndpoint {
+    return new ShowEndpoint(args);
+}
+export interface EndpointArguments extends CoreArguments {
+    id: args.StringArgument,
+}
+export class ShowEndpoint extends TfCommand<EndpointArguments, taskAgentContracts.ServiceEndpoint> {
+    protected serverCommand = true;
+    protected description = "Get a list of build definitions";
+    protected setCommandArgs(): void {
+        super.setCommandArgs();
+        this.registerCommandArgument("id", "Endpoint ID", "Endpoint Guid Identifier.", args.StringArgument, null);
+    }
+    
+    protected getHelpArgs(): string[] {
+        return ["project","id"];
+    }
+
+    public exec(): Promise<taskAgentContracts.ServiceEndpoint> {
+        var agentapi: agentClient.ITaskAgentApiBase = this.webApi.getTaskAgentApi(this.connection.getCollectionUrl());
+        var coreapi:corem.ICoreApi = this.webApi.getCoreApi(this.connection.getCollectionUrl())
+        trace.debug("Searching for Service Endpoints ...");
+        return this.commandArgs.project.val().then((project) => {
+            return this.commandArgs.id.val().then((id) =>{
+                return coreapi.getProject(project).then((projectObj) =>{               
+                    return agentapi.getServiceEndpointDetails(projectObj.id,id).then((endpoint) => {
+                        return endpoint;
+                    });
+                });
+            });
+        });
+    }
+
+    public friendlyOutput(data: taskAgentContracts.ServiceEndpoint): void {
+        if (!data) {
+            throw new Error('no definitions supplied');
+        }
+
+        trace.println();
+        trace.info('id              : %s', data.id);
+        trace.info('name            : %s', data.name);
+        trace.info('type            : %s', data.type);
+        trace.info('description     : %s', data.description);
+        trace.info('visibility      : %s', JSON.stringify(data.data));
+        trace.info('auth scheme     : %s', JSON.stringify(data.authorization.scheme));
+        trace.info('auth parameters : %s', JSON.stringify(data.authorization.parameters));
+        trace.info('created By      : %s', data.createdBy.displayName);
+    }
+}
