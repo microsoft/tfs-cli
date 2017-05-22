bd7bde467c3056063b6b5e73a4939c73218d2f2c
Shpigelman, StasX <stasx.shpigelman@intel.com>
2017-05-22 17:31:22 +0300

    Delete agent from pool

--- app/exec/build/agent.ts
+++ app/exec/build/agent.ts
@@ -11,16 +11,16 @@ export function getCommand(args: string[]): Agent {
 }
 
 export class Agent extends agentBase.BuildBase<agentBase.BuildArguments, taskAgentContracts.TaskAgent> {
-    protected serverCommand = true;
+	protected serverCommand = true;
 	protected description = "Show / Update task agent details.";
 
 	protected getHelpArgs(): string[] {
-		return ["poolId","agentId", "agentName","userCapabilityKey","userCapabilityValue","disable","parallel"];
+		return ["poolId", "agentId", "agentName", "userCapabilityKey", "userCapabilityValue", "disable", "delete", "parallel"];
 	}
 
 	public exec(): Promise<taskAgentContracts.TaskAgent> {
 		trace.debug("agent.exec");
-		var agentapi: agentClient.ITaskAgentApiBase = this.webApi.getTaskAgentApi(this.connection.getCollectionUrl().substring(0,this.connection.getCollectionUrl().lastIndexOf("/")));
+		var agentapi: agentClient.ITaskAgentApiBase = this.webApi.getTaskAgentApi(this.connection.getCollectionUrl().substring(0, this.connection.getCollectionUrl().lastIndexOf("/")));
 		return Promise.all<number | string>([
 			this.commandArgs.agentId.val(),
 			this.commandArgs.agentName.val(),
@@ -28,25 +28,26 @@ export class Agent extends agentBase.BuildBase<agentBase.BuildArguments, taskAge
 			this.commandArgs.userCapabilityKey.val(),
 			this.commandArgs.userCapabilityValue.val(),
 			this.commandArgs.disable.val(),
+			this.commandArgs.deleteAgent.val(),
 			this.commandArgs.parallel.val()
 		]).then((values) => {
-			const [agentid, agentname, pool, newkey, value, disable, maxParallel] = values;
+			const [agentid, agentname, pool, newkey, value, disable, deleteAgent, maxParallel] = values;
 			var agents: number[] = null;
-			trace.debug("getting pool  : %s",pool);
+			trace.debug("getting pool  : %s", pool);
 			trace.debug("getting agent (id) : %s", agentid);
 			trace.debug("getting agent (name) : %s", agentname);
 			var include: boolean = true;
 			if (agentid) {
 				agents = [agentid as number];
 			}
-			else if(agentname) {
+			else if (agentname) {
 				trace.debug("No agent Id provided, checking for agent with name " + agentname);
 				return agentapi.getAgents(pool as number, agentname as string).then((ao: taskAgentContracts.TaskAgent[]) => {
-					if(ao.length > 0) {
+					if (ao.length > 0) {
 						var aid = ao[0].id;
 						var an = ao[0].name;
-						trace.debug("found, agent id %s for agent name %s",aid, an);
-						return this._getOrUpdateAgent(agentapi, pool as number,aid,newkey as string,value as string,include,disable as string, maxParallel as number ); 
+						trace.debug("found, agent id %s for agent name %s", aid, an);
+						return this._getOrUpdateAgent(agentapi, pool as number, aid, newkey as string, value as string, include, disable as string, deleteAgent as string, maxParallel as number);
 					}
 					else {
 						trace.debug("No agents found with name " + agentname);
@@ -54,68 +55,85 @@ export class Agent extends agentBase.BuildBase<agentBase.BuildArguments, taskAge
 					}
 				});
 			}
-			trace.debug("disable request: %s",disable);
-			return this._getOrUpdateAgent(agentapi, pool as number, agentid as number, newkey as string, value as string, include, disable as string, maxParallel as number);	
-			});
-		};
-	
+			trace.debug("disable request: %s", disable);
+			return this._getOrUpdateAgent(agentapi, pool as number, agentid as number, newkey as string, value as string, include, disable as string, deleteAgent as string, maxParallel as number);
+		});
+	};
+
 	public friendlyOutput(agent: taskAgentContracts.TaskAgent): void {
 		if (!agent) {
 			throw new Error("agent / pool not supplied or not found");
 		}
-		trace.println();
-		trace.info("Agent id        	: %s", agent.id ? agent.id : "unknown");
-		trace.info("Agent name      	: %s", agent.name ? agent.name : "unknown");
-		trace.info("Version         	: %s", agent.version ? agent.version : "unknown");
-		trace.info("status          	: %s", agent.status ? taskAgentContracts.TaskAgentStatus[agent.status] : "unknown");
-		trace.info("enabled		      	: %s", agent.enabled ? agent.enabled : "unknown");
-		trace.info("maxParallelism		: %s", agent.maxParallelism);
-		if (agent.systemCapabilities){
-			trace.info("System capabilities : ");
+		else if (!agent.id) {
+			trace.success("Agent name          : %s", agent.name + " was deleted");
 		}
-		for (var key in agent.systemCapabilities) {
- 		    	trace.info("	%s : %s",key , agent.systemCapabilities[key]);
+		else {
+			trace.println();
+			trace.info("Agent id        	: %s", agent.id ? agent.id : "unknown");
+			trace.info("Agent name      	: %s", agent.name ? agent.name : "unknown");
+			trace.info("Version         	: %s", agent.version ? agent.version : "unknown");
+			trace.info("status          	: %s", agent.status ? taskAgentContracts.TaskAgentStatus[agent.status] : "unknown");
+			trace.info("enabled		      	: %s", agent.enabled ? agent.enabled : "unknown");
+			trace.info("maxParallelism		: %s", agent.maxParallelism);
+			if (agent.systemCapabilities) {
+				trace.info("System capabilities : ");
 			}
-		if (agent.userCapabilities) {
-			trace.info("User capabilities : ");	
-		}
-		for (var key in agent.userCapabilities) {
-				trace.info("	%s : %s", key ,agent.userCapabilities[key]);
+			for (var key in agent.systemCapabilities) {
+				trace.info("	%s : %s", key, agent.systemCapabilities[key]);
+			}
+			if (agent.userCapabilities) {
+				trace.info("User capabilities : ");
+			}
+			for (var key in agent.userCapabilities) {
+				trace.info("	%s : %s", key, agent.userCapabilities[key]);
+			}
 		}
 	}
-	private _getOrUpdateAgent(agentapi:  agentClient.ITaskAgentApiBase,pool: number,agentid: number, newkey: string, value: string, include: boolean, disable: string , Parallel: number) {
-			return agentapi.getAgent(pool,agentid,true,true,null).then((agent) => {
-			trace.debug("disable request: %s",disable);
+	private _getOrUpdateAgent(agentapi: agentClient.ITaskAgentApiBase, pool: number, agentid: number, newkey: string, value: string, include: boolean, disable: string, deleteAgent: string, Parallel: number) {
+		return agentapi.getAgent(pool, agentid, true, true, null).then((agent) => {
+			trace.debug("disable request: %s", disable);
 			if (Parallel) {
 				agent.maxParallelism = Parallel;
 				agentapi.updateAgent(agent, pool, agentid);
 			}
+			if (deleteAgent) {
+				if (deleteAgent == "true") {
+					return agentapi.deleteAgent(pool, agentid).then(() => {
+						trace.debug("agent set for deletion : %s");
+						agent.id = null;
+						return agent;
+					})
+				}
+				if (deleteAgent != "true") {
+					trace.error("allowed value is [true] only!")
+				}
+			}
 			if (disable) {
 				if (disable == "true") {
-						include = false;
-						trace.debug("agent status (enabled): %s",agent.enabled);
-						agent.enabled = false;
-						agentapi.updateAgent(agent,pool,agentid);
-						trace.debug("agent status (enabled): %s",agent.enabled);
-					}
+					include = false;
+					trace.debug("agent status (enabled): %s", agent.enabled);
+					agent.enabled = false;
+					agentapi.updateAgent(agent, pool, agentid);
+					trace.debug("agent status (enabled): %s", agent.enabled);
+				}
 				if (disable == "false") {
-						include = false;
-						trace.debug("agent status (enabled): %s",agent.enabled);
-						agent.enabled = true;
-						agentapi.updateAgent(agent,pool,agentid);
-						trace.debug("agent status (enabled): %s",agent.enabled);
-					}
+					include = false;
+					trace.debug("agent status (enabled): %s", agent.enabled);
+					agent.enabled = true;
+					agentapi.updateAgent(agent, pool, agentid);
+					trace.debug("agent status (enabled): %s", agent.enabled);
+				}
 				if (disable != "true" && disable != "false") {
-					trace.error("allowed values are [true] or [false]!")		
+					trace.error("allowed values are [true] or [false]!")
 				}
 			}
 			if (newkey) {
 				include = false;
-					var capabilities: { [key: string] : string; } = agent.userCapabilities;
-					capabilities[newkey] = value;					
-					agentapi.updateAgentUserCapabilities(capabilities,pool,agentid);
-					};
-				return agentapi.getAgent(pool,agentid,include,include,null);
-				});	
+				var capabilities: { [key: string]: string; } = agent.userCapabilities;
+				capabilities[newkey] = value;
+				agentapi.updateAgentUserCapabilities(capabilities, pool, agentid);
+			};
+			return agentapi.getAgent(pool, agentid, include, include, null)
+		});
 	}
 }
