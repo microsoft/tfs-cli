dbb6a3cab1f2591b70593045d063340ce63a31ea
Shani Feldman <grawcho@gmail.com>
2018-05-16 20:58:08 +0300

    add some stuff to PR show options - PR ID squash, autocomplete, etc.

--- app/exec/code/git/requestoptions.ts
+++ app/exec/code/git/requestoptions.ts
@@ -9,11 +9,11 @@ import VSSInterfaces = require('vso-node-api/interfaces/common/VSSInterfaces');
 import codedBase = require('./default');
 var repositoryName;
 
-export function getCommand(args: string[]): RequestDetails {
-	return new RequestDetails(args);
+export function getCommand(args: string[]): RequestOptions {
+	return new RequestOptions(args);
 }
 
-export class RequestDetails extends codedBase.CodeBase<codedBase.CodeArguments, void> {
+export class RequestOptions extends codedBase.CodeBase<codedBase.CodeArguments, void> {
 	protected serverCommand = true;
 	protected description = "Get a list of pull requests";
 
@@ -36,17 +36,33 @@ export class RequestDetails extends codedBase.CodeBase<codedBase.CodeArguments, 
 			};
 		});
 		var request = await gitApi.getPullRequest(gitRepositorie.id, +requestId);
-		return request.completionOptions;
+		var gprco = request.completionOptions as GPRCO;
+		gprco.requestId = request.pullRequestId;
+		return gprco;
 	};
 	
-	public friendlyOutput(opt: gi.GitPullRequestCompletionOptions): void {
+	public friendlyOutput(opt: GPRCO): void {
 		if (!opt) {
 			throw new Error("no pull requests supplied");
 		}		
-			trace.info('Source Branch		: %s', opt.deleteSourceBranch);
-			trace.info('Commit Message	: %s', opt.mergeCommitMessage);
-			trace.info('Squash Merge		: %s', opt.squashMerge);
-			trace.println();
+		trace.info('Request Id		: %s', opt.requestId);
+		trace.info('Bypass Reason		: %s', opt.bypassReason ? opt.bypassReason : "None");
+		trace.info('Auto Complete		: %s', opt.triggeredByAutoComplete ? opt.triggeredByAutoComplete : false);
+		trace.info('Delete Branch		: %s', opt.deleteSourceBranch ? opt.deleteSourceBranch : false);
+		trace.info('Squash Merge		: %s', opt.squashMerge ? opt.squashMerge : false);
+		trace.info('Commit Message		: _____ \n%s', opt.mergeCommitMessage ? opt.mergeCommitMessage : "No Comment");
+		trace.println();
 	}
 };
+
+
+
+class GPRCO implements gi.GitPullRequestCompletionOptions {
+	requestId: number;
+	deleteSourceBranch: boolean;
+	mergeCommitMessage: string;
+	squashMerge: boolean;
+	triggeredByAutoComplete: boolean;
+	bypassReason: string;
+}
 
